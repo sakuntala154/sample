d672f4d2942fd6c7302a72b78f237929288f7929
sakuntala154 <sakuntaladev14@gmail.com>
2017-02-19 12:26:07 -0800

    java file

---
+++ hai/SchoolStudent.java
@@ -0,0 +1,18 @@
+class SchoolStudent extends Student
+  {
+  String schnm;
+  SchoolStudent()
+     {
+     schnm="rgukt";
+      super();
+     }
+     void showData()
+       {
+        System.out.println(schnm);
+       }
+       public static void main(String args[])
+          {
+          SchoolStudent j=new SchoolStudent();
+          s.showData();
+          }
+      }
